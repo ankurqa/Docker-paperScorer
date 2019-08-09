4dd888b813df135685d2cbbe83970606653df8ac
Ankur Mangal <46736876+ankurqa@users.noreply.github.com>
2019-08-09 15:23:46 +0530

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -22,3 +22,15 @@ checkinstall
 sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 ldconfig
 echo "OpenCV" $version "ready to be used"
+
+#now in /etc/environment et these paths
+#JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+#M2_HOME=/opt/maven
+##PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$JAVA_HOME:$JRE_HOME:$M2_HOME/bin:$CATALINA_HOME$PATH"
+#JAVA_OPTS="-Djava.library.path=/opt/opencv/opencv-3.0.0/build/lib"
+#LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/opencv/opencv-3.0.0/build/lib"
+#CATALINA_HOME="/opt/tomcat/apache-tomcat-8.5.40"
+
+
+#run this command
+#source /etc/environment
